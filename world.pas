381e010777c0e57f58bb8ffdc925980dcd695147
rexim <reximkut@gmail.com>
2021-04-04 13:52:58 +0700

    Remove dependency on TPoint from World

--- world.pas
+++ world.pas
@@ -12,12 +12,13 @@ type
         Rows       : Integer;
         Cols       : Integer;
         Cells      : Array of TCell;
-        Player_Pos : TPoint;
+        Player_Row : Integer;
+        Player_Col : Integer;
     end;
 
-    function Create_World(Rows: Integer; Cols: Integer; Cell: TCell): TWorld;
-    function World_Get(World: TWorld; Point: TPoint): TCell;
-    procedure World_Set(var World: TWorld; Point: TPoint; Cell: TCell);
+    function Create_World(Rows, Cols: Integer; Cell: TCell): TWorld;
+    function World_Get(World: TWorld; Row, Col: Integer): TCell;
+    procedure World_Set(var World: TWorld; Row, Col: Integer; Cell: TCell);
     procedure World_Fill_Rect(var World: TWorld; Rect: TRect; Cell: TCell);
 
     procedure World_Render(World: TWorld);
@@ -25,18 +26,14 @@ type
 
 const
     Cell_To_Char: array[TCell] of Char = (' ', '.', '|', '-', '+', '#');
-    Dir_To_Offset: array[TDir] of TPoint = (
-        (Row: -1; Col:  0),
-        (Row:  1; Col:  0),
-        (Row:  0; Col:  1),
-        (Row:  0; Col: -1)
-    );
+    Row_Offset: array[TDir] of Integer = (-1, 1, 0, 0);
+    Col_Offset: array[TDir] of Integer = (0, 0, 1, -1);
     Walkable: array[TCell] of Boolean = (False, True, False, False, True, True);
 
 implementation
     uses Math;
 
-    function Create_World(Rows: Integer; Cols: Integer; Cell: TCell): TWorld;
+    function Create_World(Rows, Cols: Integer; Cell: TCell): TWorld;
     var
         Index : Integer;
         Result : TWorld;
@@ -48,18 +45,19 @@ implementation
         begin
             Result.Cells[Index] := Cell;
         end;
-        Result.Player_Pos := Make_Point(0, 0);
+        Result.Player_Row := 0;
+        Result.Player_Col := 0;
         Create_World := Result
     end;
 
-    function World_Get(World: TWorld; Point: TPoint): TCell;
+    function World_Get(World: TWorld; Row, Col: Integer): TCell;
     begin
-        World_Get := World.Cells[Modulo(Point.Row, World.Rows) * World.Cols + Modulo(Point.Col, World.Cols)];
+        World_Get := World.Cells[Modulo(Row, World.Rows) * World.Cols + Modulo(Col, World.Cols)];
     end;
 
-    procedure World_Set(var World: TWorld; Point: TPoint; Cell: TCell);
+    procedure World_Set(var World: TWorld; Row, Col: Integer; Cell: TCell);
     begin
-        World.Cells[Modulo(Point.Row, World.Rows) * World.Cols + Modulo(Point.Col, World.Cols)] := Cell;
+        World.Cells[Modulo(Row, World.Rows) * World.Cols + Modulo(Col, World.Cols)] := Cell;
     end;
 
     procedure World_Fill_Rect(var World: TWorld; Rect: TRect; Cell: TCell);
@@ -70,7 +68,7 @@ implementation
         begin
             for Col := Min(Rect.Point1.Col, Rect.Point2.Col) to Max(Rect.Point1.Col, Rect.Point2.Col)do
             begin
-                World_Set(World, Make_Point(Row, Col), Cell);
+                World_Set(World, Row, Col, Cell);
             end;
         end;
     end;
@@ -83,10 +81,10 @@ implementation
         begin
             for Col := 0 to World.Cols - 1 do
             begin
-                if (Row = World.Player_Pos.Row) and (Col = World.Player_Pos.Col) then
+                if (Row = World.Player_Row) and (Col = World.Player_Col) then
                     Write('@')
                 else
-                    Write(Cell_To_Char[World_Get(World, Make_Point(Row, Col))]);
+                    Write(Cell_To_Char[World_Get(World, Row, Col)]);
             end;
             WriteLn();
         end;
@@ -94,10 +92,14 @@ implementation
 
     procedure World_Move_Player(var World: TWorld; Dir: TDir);
     var
-        New_Pos : TPoint;
+        New_Row, New_Col: Integer;
     begin
-        New_Pos := Point_Add(World.Player_Pos, Dir_To_Offset[Dir]);
-        if Walkable[World_Get(World, New_Pos)] then
-            World.Player_Pos := New_Pos;
+        New_Row := World.Player_Row + Row_Offset[Dir];
+        New_Col := World.Player_Col + Col_Offset[Dir];
+        if Walkable[World_Get(World, New_Row, New_Col)] then
+        begin
+            World.Player_Row := New_Row;
+            World.Player_Col := New_Col;
+        end;
     end;
 end.
